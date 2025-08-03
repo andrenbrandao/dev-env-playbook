1739d7874a1db6cae5d8805e3a2062bd20bdc96c
André Brandão <andrenbrandao@gmail.com>
2025-08-03 17:58:48 -0300

    Replace ansible-pull with git clone and ansible-playbook.
    ansible-pull does not work iteratively, so it can't ask for the vault
    pass in an installation.

--- ubuntu.sh
+++ ubuntu.sh
@@ -5,6 +5,13 @@ sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update -y
 sudo apt-get install -y curl git software-properties-common ansible
 
+# Clone repo
+echo "Cloning the dev-env-playbook repository..."
+git clone https://github.com/andrenbrandao/dev-env-playbook.git /tmp/dev-env-playbook
+
 # Pull ansible and run local.yml playbook
-ansible-pull -U https://github.com/andrenbrandao/dev-env-playbook.git --ask-vault-pass
+pushd /tmp/dev-env-playbook
+echo "Running the local playbook..."
+ansible-playbook local.yml --ask-vault-pass
+popd
 
